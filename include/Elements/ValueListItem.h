caa00f5cab2c94f63f8a1fccb400cb782f62afb7
Sun <gabehttps@gmail.com>
2020-02-28 00:36:03 -0800

    use ListItem inheritance

--- include/Elements/ValueListItem.h
+++ include/Elements/ValueListItem.h
@@ -2,25 +2,25 @@
 #include <string>
 #include <tesla.hpp>
 
-class ValueListItem : public tsl::elm::Element
+class ValueListItem : public tsl::elm::ListItem
 {
 public:
-    ValueListItem(std::string text, const std::vector<std::string> values, int defaultPos, const std::string data) : tsl::elm::Element(), m_text(text), m_values(values), m_curValue(defaultPos), m_extdata(data) {}
+    ValueListItem(std::string text, const std::vector<std::string> values, int defaultPos, const std::string data) : tsl::elm::ListItem(text), m_values(values), m_curValue(defaultPos), m_extdata(data) {}
     ~ValueListItem() {}
 
     virtual void draw(tsl::gfx::Renderer *renderer) override
     {
         if (this->m_valueWidth == 0)
         {
-            auto [width, height] = renderer->drawString(m_values.at(m_curValue).c_str(), false, 0, 0, 20, tsl::style::color::ColorTransparent);
+            auto [width, height] = renderer->drawString(this->m_values.at(this->m_curValue).c_str(), false, 0, 0, 20, tsl::style::color::ColorTransparent);
             this->m_valueWidth = width;
         }
 
         renderer->drawRect(this->getX(), this->getY(), this->getWidth(), 1, a({0x5, 0x5, 0x5, 0xF}));
         renderer->drawRect(this->getX(), this->getY() + this->getHeight(), this->getWidth(), 1, a({0x5, 0x5, 0x5, 0xF}));
 
-        renderer->drawString(m_text.c_str(), false, this->getX() + 20, this->getY() + 45, 23, a({0xF, 0xF, 0xF, 0xF}));
-        renderer->drawString(m_values.at(m_curValue).c_str(), false, this->getX() + this->getWidth() - this->m_valueWidth - 20, this->getY() + 45, 20, this->m_faint ? a({0x6, 0x6, 0x6, 0xF}) : a({0x5, 0xC, 0xA, 0xF}));
+        renderer->drawString(this->m_text.c_str(), false, this->getX() + 20, this->getY() + 45, 23, a({0xF, 0xF, 0xF, 0xF}));
+        renderer->drawString(this->m_values.at(m_curValue).c_str(), false, this->getX() + this->getWidth() - this->m_valueWidth - 20, this->getY() + 45, 20, this->m_faint ? a({0x6, 0x6, 0x6, 0xF}) : a({0x5, 0xC, 0xA, 0xF}));
     }
 
     virtual bool onClick(u64 keys) override
@@ -39,24 +39,12 @@ public:
         return false;
     }
 
-    virtual Element *requestFocus(Element *oldFocus, tsl::FocusDirection direction) override
-    {
-        return this;
-    }
-
-    virtual inline void setText(std::string text) final
-    {
-        this->m_text = text;
-    }
-
     virtual inline void setValue(int curValue, bool faint = false) final
     {
         this->m_curValue = curValue;
         this->m_faint = faint;
         this->m_valueWidth = 0;
     }
-
-    virtual void layout(u16 parentX, u16 parentY, u16 parentWidth, u16 parentHeight) override {}
 
     int getCurValue() { return this->m_curValue; }
     void setCurValue(int value) { this->m_curValue = value; }
@@ -66,10 +54,7 @@ public:
     const std::vector<std::string> getValues() { return this->m_values; }
 
 protected:
-    std::string m_text;
     const std::vector<std::string> m_values;
     int m_curValue;
     const std::string m_extdata;
-    bool m_faint = false;
-    u16 m_valueWidth = 0;
 };
