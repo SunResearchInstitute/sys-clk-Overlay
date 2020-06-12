c066e5bab38ba79eb215fa5a7d8efa99105afae9
HookedBehemoth <Luisscheurenbrand@gmail.com>
2020-06-12 20:26:43 +0200

    fix crash

--- source/Elements/ClkConfigListItem.cpp
+++
@@ -1,7 +0,0 @@
-#include "ClkConfigListItem.h"
-#include "clk.h"
-
-ClkConfigListItem::ClkConfigListItem(std::string text, const std::vector<std::string> &values, const std::string &configName) : ValueListItem(text, values), ConfigName(configName) { m_curValue = Utils::clk::getConfigValuePos(this->m_values, this->ConfigName); }
-ClkConfigListItem::~ClkConfigListItem() { Utils::clk::ChangeConfiguration(this); }
-
-void ClkConfigListItem::setConfigWithCurValue() { ClkConfigListItem::setCurValue(Utils::clk::getConfigValuePos(this->getValues(), this->getConfigName())); }
